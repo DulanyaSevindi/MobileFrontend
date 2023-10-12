26714b9cb4a3af4ca3d2b259ea410576bc6e9b12
IT21277436 <it21277436@my.sliit.lk>
2023-10-13 02:10:26 +0530

    Tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./App/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./App/**/*.{js,jsx,ts,tsx}",
+    "./**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
